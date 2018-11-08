65667a39870c402588eb4e46657d55d09e015da4
seangnewman <seangnewman@msn.com>
2018-11-08 22:12:06 +0000

    capture code

---
+++ ChatbotSolo20-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=chatbotsolo20-Web-Deploy.pubxml -p:Password=3ipubTEAGJBdMP1w3RqfkBYBbvf7F4dBRnwZjefxu8xQbNZGxsnGgNhag70m
+
---
+++ SOLO2KB-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=solo2kb-Web-Deploy.pubxml -p:Password=MMy4GbKyusggjluiFH2oHX1LzGaYh9rhciFzRgeF5jiBKF5Xs9KGciE9XNL7
+
